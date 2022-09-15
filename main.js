d326ea63b67392224a2a71bcb878bd5c33a7df06
Wai-Tung-Wong <113068760+Wai-Tung-Wong@users.noreply.github.com>
2022-09-14 20:10:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/*import { halfof } from "./lib.js";
+console.log(halfof(84));
+
+import { multiply } from "./lib.js";
+console.log(multiply(10, 2));
+
+let x = multiply(3, 2);
+console.log(x);*/
+
+import { halfof, multiply } from "./lib.js";
+console.log(halfof(18));
+console.log(multiply(1,99));
+
+import lib from "./lib.js";
+lib();
